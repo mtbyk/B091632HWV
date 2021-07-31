eb968864be6072349a6ae3afcc4285f9c873b49d
mtbyk <af15081@shibaura-it.ac.jp>
2021-08-01 01:50:30 +0900

    sample

---
+++ Sample/Sample/Sample.cpp
@@ -0,0 +1,19 @@
+#include <ntddk.h>
+
+void SampleUnload(_In_ PDRIVER_OBJECT DriverObject) {
+	UNREFERENCED_PARAMETER(DriverObject);
+
+	KdPrint(("unloaded\n"));
+}
+
+extern "C"
+NTSTATUS
+DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath) {
+	UNREFERENCED_PARAMETER(RegistryPath);
+
+	DriverObject->DriverUnload = SampleUnload;
+
+	KdPrint(("loaded\n"));
+
+	return STATUS_SUCCESS;
+}
