31dd3db1570bcfed2c648f27ed3e20503e0a8d6e
mtbyk <af15081@shibaura-it.ac.jp>
2021-08-01 17:59:41 +0900

    CreateとCloseのディスパッチルーチン

--- Sample/PriorityBooster/PriorityBooster.cpp
+++ Sample/PriorityBooster/PriorityBooster.cpp
@@ -1,5 +1,11 @@
 #include <ntddk.h>
 
+NTSTATUS PriorityBoosterCreateClose(_In_ PDEVICE_OBJECT DeviceObject, _In_ PIRP Irp) {
+	UNREFERENCED_PARAMETER(Irp);
+	UNREFERENCED_PARAMETER(DeviceObject);
+	return STATUS_SUCCESS;
+}
+
 void PriorityBoosterUnload(_In_ PDRIVER_OBJECT DriverObject) {
 	UNREFERENCED_PARAMETER(DriverObject);
 
@@ -12,6 +18,8 @@ DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath)
 	UNREFERENCED_PARAMETER(RegistryPath);
 
 	DriverObject->DriverUnload = PriorityBoosterUnload;
+	DriverObject->MajorFunction[IRP_MJ_CREATE] = PriorityBoosterCreateClose;
+	DriverObject->MajorFunction[IRP_MJ_CLOSE] = PriorityBoosterCreateClose;
 
 	RTL_OSVERSIONINFOW info;
 	RtlGetVersion(&info);
