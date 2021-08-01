26fcc7707076c9249962379963ea6b8f6cd98153
mtbyk <af15081@shibaura-it.ac.jp>
2021-08-01 17:40:24 +0900

    エントリとアンロード

---
+++ Sample/PriorityBooster/PriorityBooster.cpp
@@ -0,0 +1,21 @@
+#include <ntddk.h>
+
+void PriorityBoosterUnload(_In_ PDRIVER_OBJECT DriverObject) {
+	UNREFERENCED_PARAMETER(DriverObject);
+
+	KdPrint(("unloaded\n"));
+}
+
+extern "C"
+NTSTATUS
+DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath) {
+	UNREFERENCED_PARAMETER(RegistryPath);
+
+	DriverObject->DriverUnload = PriorityBoosterUnload;
+
+	RTL_OSVERSIONINFOW info;
+	RtlGetVersion(&info);
+	KdPrint(("majar: %lu\nminor: %lu\nbuild: %lu\n", info.dwMajorVersion, info.dwMinorVersion, info.dwBuildNumber));
+
+	return STATUS_SUCCESS;
+}
