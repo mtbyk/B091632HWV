74ae82d480d04e649bea92b9e6b8daf7b7667e93
mtbyk <af15081@shibaura-it.ac.jp>
2021-08-01 19:20:55 +0900

    デバイスオブジェクトとシンボリックリンクの作成

--- Sample/PriorityBooster/PriorityBooster.cpp
+++ Sample/PriorityBooster/PriorityBooster.cpp
@@ -7,7 +7,9 @@ NTSTATUS PriorityBoosterCreateClose(_In_ PDEVICE_OBJECT DeviceObject, _In_ PIRP 
 }
 
 void PriorityBoosterUnload(_In_ PDRIVER_OBJECT DriverObject) {
-	UNREFERENCED_PARAMETER(DriverObject);
+	UNICODE_STRING symLink = RTL_CONSTANT_STRING(L"\\??\\PriorityBooster");
+	IoDeleteSymbolicLink(&symLink);
+	IoDeleteDevice(DriverObject->DeviceObject);
 
 	KdPrint(("unloaded\n"));
 }
@@ -21,9 +23,28 @@ DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath)
 	DriverObject->MajorFunction[IRP_MJ_CREATE] = PriorityBoosterCreateClose;
 	DriverObject->MajorFunction[IRP_MJ_CLOSE] = PriorityBoosterCreateClose;
 
-	RTL_OSVERSIONINFOW info;
-	RtlGetVersion(&info);
-	KdPrint(("majar: %lu\nminor: %lu\nbuild: %lu\n", info.dwMajorVersion, info.dwMinorVersion, info.dwBuildNumber));
+	UNICODE_STRING devName = RTL_CONSTANT_STRING(L"\\Device\\PriorityBooster");
+	PDEVICE_OBJECT DeviceObject;
+	NTSTATUS status = IoCreateDevice(
+		DriverObject,
+		0,
+		&devName,
+		FILE_DEVICE_UNKNOWN,
+		0,
+		FALSE,
+		&DeviceObject
+	);
+	if (!NT_SUCCESS(status)) {
+		KdPrint(("Failed to create device object (0x%08X)\n", status));
+		return status;
+	}
+	UNICODE_STRING symLink = RTL_CONSTANT_STRING(L"\\??\\PriorityBooster");
+	status = IoCreateSymbolicLink(&symLink, &devName);
+	if (!NT_SUCCESS(status)) {
+		KdPrint(("Failed to create symbolic link (0x%08X)\n", status));
+		IoDeleteDevice(DeviceObject);
+		return status;
+	}
 
 	return STATUS_SUCCESS;
 }
