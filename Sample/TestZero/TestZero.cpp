5e9ab020a21580db3305c72893b0ab16a691ae0c
mtbyk <af15081@shibaura-it.ac.jp>
2021-08-10 21:58:53 +0900

    ヌルデバイスの作成と動確まで

---
+++ Sample/TestZero/TestZero.cpp
@@ -0,0 +1,62 @@
+﻿// TestZero.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <windows.h>
+#include <stdio.h>
+#include "../PriorityBooster/PriorityBoosterCommon.h"
+
+int Error(const char* message) {
+	printf("%s (error=%d)\n", message, GetLastError());
+	return 1;
+}
+
+int main()
+{
+	HANDLE hDevice = CreateFile(L"\\\\.\\Zero", GENERIC_READ | GENERIC_WRITE, 0, nullptr, OPEN_EXISTING, 0, nullptr);
+	if (hDevice == INVALID_HANDLE_VALUE) {
+		return Error("Failed to open device");
+	}
+    
+	BYTE buffer[64];
+
+	for (int i = 0; i < sizeof(buffer); i++) {
+		buffer[i] = i + 1;
+	}
+	DWORD bytes;
+	BOOL ok = ::ReadFile(hDevice, buffer, sizeof(buffer), &bytes, nullptr);
+	if (!ok) {
+		return Error("failed to read");
+	}
+	if (bytes != sizeof(buffer)) {
+		printf("Wrong Number of bytes\n");
+	}
+	long total = 0;
+	for (auto n : buffer) {
+		total += n;
+	}
+	if (total != 0) {
+		printf("Wrong data\n");
+	}
+
+	BYTE buffer2[1024];
+	ok = ::WriteFile(hDevice, buffer2, sizeof(buffer), &bytes, nullptr);
+	if (!ok) {
+		return Error("failed to write");
+	}
+	if (bytes != sizeof(buffer)) {
+		printf("Wrong Number of bytes\n");
+	}
+
+	::CloseHandle(hDevice);
+}
+
+// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
+// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
+
+// 作業を開始するためのヒント: 
+//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
+//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
+//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
+//   4. エラー一覧ウィンドウを使用してエラーを表示します
+//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
+//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
---
+++ Sample/Zero/Zero.cpp
@@ -0,0 +1,89 @@
+#include "pch.h"
+#define DRIVER_PREFIX "Zero: "
+
+//DriverEntry
+
+NTSTATUS CompleteIrp(PIRP Irp, NTSTATUS status = STATUS_SUCCESS, ULONG_PTR info = 0) {
+	Irp->IoStatus.Status = status;
+	Irp->IoStatus.Information = info;
+	IoCompleteRequest(Irp, 0);
+	return status;
+}
+
+NTSTATUS ZeroRead(PDEVICE_OBJECT, PIRP Irp) {
+	auto stack = IoGetCurrentIrpStackLocation(Irp);
+	auto len = stack->Parameters.Read.Length;
+	if (len == 0) {
+		return CompleteIrp(Irp, STATUS_INSUFFICIENT_RESOURCES);
+	}
+	auto buffer = MmGetSystemAddressForMdlSafe(Irp->MdlAddress, NormalPagePriority);
+	if (!buffer) {
+		return CompleteIrp(Irp, STATUS_INSUFFICIENT_RESOURCES);
+	}
+	memset(buffer, 0, len);
+
+	return CompleteIrp(Irp, STATUS_SUCCESS, len);
+}
+
+NTSTATUS ZeroWrite(PDEVICE_OBJECT, PIRP Irp) {
+	auto stack = IoGetCurrentIrpStackLocation(Irp);
+	auto len = stack->Parameters.Write.Length;
+
+	return CompleteIrp(Irp, STATUS_SUCCESS, len);
+}
+
+void ZeroUnload(_In_ PDRIVER_OBJECT DriverObject) {
+	UNICODE_STRING symLink = RTL_CONSTANT_STRING(L"\\??\\Zero");
+	IoDeleteSymbolicLink(&symLink);
+	IoDeleteDevice(DriverObject->DeviceObject);
+
+	KdPrint(("unloaded\n"));
+}
+
+NTSTATUS ZeroCreateClose(_In_ PDEVICE_OBJECT DeviceObject, _In_ PIRP Irp) {
+	UNREFERENCED_PARAMETER(DeviceObject);
+	Irp->IoStatus.Status = STATUS_SUCCESS;
+	Irp->IoStatus.Information = 0;
+	IoCompleteRequest(Irp, IO_NO_INCREMENT);
+	return STATUS_SUCCESS;
+}
+
+extern "C" NTSTATUS
+
+DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) {
+	UNREFERENCED_PARAMETER(RegistryPath);
+
+	DriverObject->DriverUnload = ZeroUnload;
+	DriverObject->MajorFunction[IRP_MJ_CREATE] = DriverObject->MajorFunction[IRP_MJ_CLOSE] = ZeroCreateClose;
+	DriverObject->MajorFunction[IRP_MJ_READ] = ZeroRead;
+	DriverObject->MajorFunction[IRP_MJ_WRITE] = ZeroWrite;
+	
+	UNICODE_STRING devName = RTL_CONSTANT_STRING(L"\\Device\\Zero");
+	UNICODE_STRING symLink = RTL_CONSTANT_STRING(L"\\??\\Zero");
+	PDEVICE_OBJECT DeviceObject = nullptr;
+	auto status = STATUS_SUCCESS;
+
+	do {
+		status = IoCreateDevice(DriverObject, 0, &devName, FILE_DEVICE_UNKNOWN, 0, FALSE, &DeviceObject);
+		if (!NT_SUCCESS(status)) {
+			KdPrint((DRIVER_PREFIX "failed to create device (1x%08X)\n", status));
+			break;
+		}
+
+		DeviceObject->Flags |= DO_DIRECT_IO;
+
+		status = IoCreateSymbolicLink(&symLink, &devName);
+		if (!NT_SUCCESS(status)) {
+			KdPrint((DRIVER_PREFIX "failed to create symbolic link (1x%08X)\n", status));
+		}
+	} while (FALSE);
+
+	if (!NT_SUCCESS(status)) {
+		if (DeviceObject) {
+			IoDeleteDevice(DeviceObject);
+		}
+	}
+
+	return status;
+}
+
